c86bb03236d9ff72c7eed9f7332ac8c313b20a6f
adnanhashmi09 <hashmi.adnan791@gmail.com>
2021-06-10 11:16:06 +0530

    second commit

--- app.js
+++ app.js
@@ -78,6 +78,10 @@ app.get('/view', (req, res) => {
 
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 app.listen(process.env.PORT || 8282, () => {
   console.log(`listening on port ${process.env.PORT || 8282}`);
